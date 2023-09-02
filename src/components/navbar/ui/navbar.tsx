52b5f97522118d202e609c04a1cb5628bf045041
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-09-02 18:00:59 +0300

    feat: confert svg to react component & add navbar component

--- src/components/navbar/ui/navbar.tsx
+++ src/components/navbar/ui/navbar.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import styles from './navbar.module.scss';
-import { Filter, Loupe, Oval, Plus } from '../../icons/icons';
+import { Filter, Plus, Search, Small } from '../../icons/ui/icons';
 
 
 export const Navbar = () => {
+
 	return (
-		<div className={ styles.navbar__container }>
-			<div className={styles.navbar__container2}>
-				<Oval className={ styles.navbar__searchOval } />
-				<Loupe className={ styles.navbar__searchLoupe } />
+		<div className={ styles.navbar__wrapper }>
+
+			<div className={ styles.navbar__container }>
+				<Search />
+				<div>
+					<Filter />
+					<Plus />
+				</div>
+			</div>
+
+			<div className={ styles.navbar__toolbar }>
+				<Small />
+				<div>Выбрать</div>
 			</div>
 
 
-
-			<div className={ 'styles.navbar__searchRight' }>
-				<Filter />
-				<Plus />
-			</div>
 		</div>
 
 	);
--- src/pages/main-page.tsx
+++ src/pages/main-page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Header } from '../components/header';
 import { Layout } from '../components/layout';
-import { Navbar } from '../components/navbar/ui/navbar';
+import { Navbar } from '../components/navbar';
 
 const MainPage = () => {
 	return (
