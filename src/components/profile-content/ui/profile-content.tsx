8b0b77e2917f2163e2cbbdfa950ee8e1244bb654
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-09-03 23:37:02 +0300

    feat: add video component

--- src/components/profile-content/ui/profile-content.tsx
+++ src/components/profile-content/ui/profile-content.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './profileContent.module.scss'
 import { Notes } from '../../notes';
 import { Consult } from '../../consult';
+import { Video } from '../../video';
 
 
 export const ProfileContent = ({ activeButton }: {
@@ -14,7 +15,7 @@ export const ProfileContent = ({ activeButton }: {
 			case 'Консультации':
 				return <Consult />;
 			case 'Видео':
-				return 'Видео content';
+				return <Video />;
 			case 'Мероприятия':
 				return 'Мероприятия content';
 			default:
---
+++ src/components/video/ui/video.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import video1 from '../../../assets/video1.svg'
+import video2 from '../../../assets/video2.svg'
+import video3 from '../../../assets/video3.svg'
+
+
+import styles from './video.module.scss'
+
+export const Video = () => {
+	return (
+		<div className={ styles.container }>
+			<div className={ `${styles.content__container}` }>
+				<div className={ styles.content__leftContainer }>
+					<img src={ video1 } alt={ 'video1' } />
+					<div className={ styles.content__subcontainer }>
+						<div className={ styles.content__name }>Крабик, ходьба в бок в приседе с двумя резинками Кра…</div>
+						<div className={ styles.content__dop }>Астахова Е.В.</div>
+					</div>
+				</div>
+			</div>
+
+			<div className={ styles.content__container }>
+				<div className={ styles.content__leftContainer }>
+					<img src={ video2 } alt={ 'video2' } />
+					<div className={ styles.content__subcontainer }>
+						<div className={ styles.content__name }>Разминка для локтевого сустава</div>
+						<div className={ styles.content__dop }>Астахова Е.В.</div>
+					</div>
+					<div className={ styles.content__rightContainer }>
+						<div className={ styles.content__status }>15.01.2019 - 22.01.2019</div>
+					</div>
+				</div>
+			</div>
+
+			<div className={ styles.content__container }>
+				<div className={ styles.content__leftContainer }>
+					<div className={ styles.imageBackground }>
+						<img src={ video3 } alt={ 'video3' } />
+					</div>
+
+					<div className={ styles.content__subcontainer }>
+						<div className={ styles.content__name }>Разминка для локтевого суставаРазминка для локтевого..</div>
+						<div className={ styles.content__dop }>Астахова Е.В.</div>
+					</div>
+
+					<div className={ styles.content__rightContainer }>
+						<div className={ styles.content__status }>15.01.2019 - 22.01.2019</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	);
+};
+
